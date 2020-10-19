b763bfe1071c6c22d5f2856a71db9c1a021aff8d
fabricioveronez <fabricioveronez@hotmail.com>
2020-10-19 15:27:57 -0300

    Mudando a versão

--- src/controllers/produto.js
+++ src/controllers/produto.js
@@ -41,7 +41,7 @@ exports.productAll = function (req, res) {
             return res.json(err);
         }       
 
-        var retu = { product, machine: os.hostname(), version: 'v2' };
+        var retu = { product, machine: os.hostname() };
 
         res.json(retu);
     })
