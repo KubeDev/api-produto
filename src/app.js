f7723112e01cbf627de901e9bcaa8419892d3cf3
fabricioveronez <fabricioveronez@hotmail.com>
2021-02-14 00:03:48 -0300

    Metricas

--- src/app.js
+++ src/app.js
@@ -8,35 +8,43 @@ const YAML = require('yamljs');
 const swaggerDocument = YAML.load('./swagger.yaml');
 const NodeHog = require('nodehog');
 const config = require('./config/system-life');
-const apiMetrics = require('prometheus-api-metrics');
+const promBundle = require("express-prom-bundle");
 
-app.use(apiMetrics())
+const metricsMiddleware = promBundle({ 
+        includeMethod: true, 
+        includePath: true, 
+        customLabels: 
+            { 
+                project_version: '2.0' 
+            } 
+    });
+
+app.use(metricsMiddleware);
 app.use(config.middlewares.healthMid);
 app.use('/config', config.routers);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument)); 
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const serverStatus = () => {
-    return { 
-       state: 'up', 
-       dbState: mongoose.STATES[mongoose.connection.readyState] 
+    return {
+        state: 'up',
+        dbState: mongoose.STATES[mongoose.connection.readyState]
     }
-  };
+};
 
-app.get('/health', (res, req) => {  
-    let healthResult = serverStatus();   
+app.get('/health', (res, req) => {
+    let healthResult = serverStatus();
     if (mongoose.connection.readyState == 0) {
         req.statusCode = 500;
-        req.send('down');        
+        req.send('down');
     } else {
         req.json(healthResult);
-    }        
+    }
 });
 
 app.get('/stress/:elemento/tempostress/:tempoStress/tempofolga/:tempoFolga/ciclos/:ciclos', (req, res) => {
-    
+
     const elemento = req.params.elemento;
     const tempoStress = req.params.tempoStress * 1000;
     const tempoFolga = req.params.tempoFolga * 1000;
@@ -52,15 +60,15 @@ var mongoUrl = process.env.MONGODB_URI || developer_db_url;
 
 mongoose.Promise = global.Promise;
 
-var connectWithRetry = function() {
-    return mongoose.connect(mongoUrl, function(err) {
+var connectWithRetry = function () {
+    return mongoose.connect(mongoUrl, function (err) {
         if (err) {
             console.error('Failed to connect to mongo on startup - retrying in 5 sec', err);
             setTimeout(connectWithRetry, 5000);
         }
     });
 };
-  
+
 connectWithRetry();
 
 var port = process.env.SERVER_PORT || 8080;
